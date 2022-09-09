a75cb8260a73b0a5eb64df08abdb21f4540ef4d7
Richard Strickler <richastrick1998@gmail.com>
2022-09-09 09:26:48 -0500

    intitial

---
+++ src/main/java/com/training/assignment/restPlus/RestPlusApplication.java
@@ -0,0 +1,13 @@
+package com.training.assignment.restPlus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/training/assignment/restPlus/RestPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.assignment.restPlus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
